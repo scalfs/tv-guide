8c22501839c490fdc00897b4ff5399707ee29073
Vitor Escalfoni <vitor.hme@gmail.com>
2022-09-11 10:38:31 -0300

    chore: added babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ".",
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@config": "./src/config",
+            "@hooks": "./src/hooks",
+            "@navigation": "./src/navigation",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@theme": "./src/theme",
+            "@types": "./src/types",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
